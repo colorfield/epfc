df3ed9acec5a0fee86a77805978a79a4164dc751
Christophe <christophe@webair.local>
2014-05-11 12:13:03 +0200

    [refactoring] encapsulation for disk and partition classes

--- concours2014/answer/partition.js
+++ concours2014/answer/partition.js
@@ -1,4 +1,6 @@
-// Partition class
+/**
+ * Partition class
+ */
 function Partition(){
 	this.size = 0;
 	this.isFirst = false;
@@ -6,48 +8,63 @@ function Partition(){
 	this.limits = [];
 }
 
-// disk object
-var disk = {
-    size: 10,
-    nPartitions: 3,
-    limits: [],
-    partitions: []
+/**
+ * Disk class
+ */
+function Disk(){
+  this.size = 0;
+  this.nCuts =  0;
+  this.limits = [];
+  this.partitions = [];
+
+  /**
+   * Create partitions objects an a disk object
+   */
+  this.createPartitions = function(){
+    // create partitions and calculate each size
+    for (var limitKey in this.limits){
+     console.log(this.limits[limitKey]);
+     var p = new Partition();
+     // exception 1 : is first partition
+     if(limitKey == 0){
+      p.isFirst = true;
+      p.size = this.limits[limitKey];
+      p.limits = [0, this.limits[limitKey]];
+      console.log('is FIRST');
+     // in between, do nothing, initialized to false
+     }else{
+      p.size = this.limits[limitKey] - this.limits[limitKey - 1];
+      p.limits = [this.limits[limitKey - 1], this.limits[limitKey]];
+     }
+     this.partitions.push(p);
+    }
+
+    // exception 2 : is last partition
+    var p = new Partition();
+    p.isLast = true;
+    p.size = this.size - this.limits[this.limits.length - 1];
+    p.limits = [this.limits[limitKey], this.size - 1];
+    console.log('is LAST');
+    this.partitions.push(p);
+
+    console.log(this.partitions);
+  }
 };
 
 // main
 $( document ).ready(function() {
-  // input = disk size and limits
-  $("#result").text('disk size = ' + disk.size);
+  // input 1: disk size
+  var disk = new Disk();
+  disk.size = 10;
+  // input 2: partitions cuts on the disk
+  disk.nCuts = 3;
+  // input 3: partitions limits on the disk
   disk.limits = [2,4,7];
 
-  // create partitions and calculate each size
-  for (var limitKey in disk.limits){
-   console.log(disk.limits[limitKey]);
-   var p = new Partition();
-   // exception 1 : is first partition
-   if(limitKey == 0){
-   	p.isFirst = true;
-   	p.size = disk.limits[limitKey];
-    p.limits = [0, disk.limits[limitKey]];
-   	console.log('is FIRST');
-   // in between, do nothing, initialized to false
-   }else{
-   	p.size = disk.limits[limitKey] - disk.limits[limitKey - 1];
-    p.limits = [disk.limits[limitKey - 1], disk.limits[limitKey]];
-   }
-   
-   disk.partitions.push(p);
+  disk.createPartitions();
 
-  }
-
-  // exception 2 : is last partition
-  var p = new Partition();
-  p.isLast = true;
-  p.size = disk.size - disk.limits[disk.limits.length - 1];
-  p.limits = [disk.limits[limitKey], disk.size - 1];
-  console.log('is LAST');
-  disk.partitions.push(p);
-
-  console.log(disk.partitions);
+  $("#result").text('Disk size = ' + disk.size);
+  
+  
 
 });
